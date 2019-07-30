77a1d3d1d99e90a7a26b62b730c5b10043a0207d
Ethan Collins <ethantcollins98@gmail.com>
2019-07-29 18:03:36 -0600

    updated auth.rb, class method access_token

---
+++ lib/auth.rb
@@ -0,0 +1,14 @@
+require 'rest-client'
+require 'pry'
+require 'json'
+
+ class GetData
+    @@base_url = "https://api.spotify.com/v1"
+    def self.access_token
+        token = RestClient.post('https://accounts.spotify.com/api/token',
+        {'grant_type': 'client_credentials'},
+        {'Authorization': 'Basic YzhjZTljY2NiMDczNDg2YmE5OTJkNGUyOTM0NzBhNzA6N2ZmYTRlY2Q4Yjk1NDQ2OGJlMTI2ZGZiZDY1MGU1NWM='})
+
+        JSON.parse(token)['access_token']
+    end
+ end
